5b85e55e31dc4b1c37f84f41f103fd9c5fa11d29
colafson <olafsocm@gmail.com>
2021-05-05 04:07:13 +0000

    debug is set to false

--- virtual_sommolier_app.py
+++ virtual_sommolier_app.py
@@ -58,4 +58,4 @@ def ForestClassifier(tree, header, instance):
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
